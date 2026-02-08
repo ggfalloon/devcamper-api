a92791d6b163414f6bdd24d90ad8e37ba120ecee
ggfalloon <ggfalloon@gmail.com>
2026-02-08 15:13:17 -0600

    course routes and controller

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const errorHandler = require('./middleware/error');
-
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -14,7 +13,7 @@ connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
-const { connect } = require('mongoose');
+const courses = require('./routes/courses');
 
 const app = express();
 
@@ -30,6 +29,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
 
