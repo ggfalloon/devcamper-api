d9563330e20807fdb23432f73a81e0cf7c2d9a3c
ggfalloon <ggfalloon@gmail.com>
2026-02-09 17:45:27 -0600

    use advanced Results middleware for reuse of filters

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,9 @@ const {
     bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resourse routers
 const courseRouter = require('./courses');
 
@@ -20,9 +23,10 @@ router.use('/:bootcampId/courses', courseRouter);
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius); 
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
+
 router
     .route('/')
-    .get(getBootcamps)
+    .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
     .post(createBootcamp);
 
 router
