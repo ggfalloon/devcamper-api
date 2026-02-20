25769397f6793644008a1d40fe78331a4f5cd706
ggfalloon <ggfalloon@gmail.com>
2026-02-20 10:22:20 -0600

    create reviews model and get route

---
+++ routes/reviews.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { 
+    getReviews,
+    // getReview,
+    // createReview,
+    // updateReview,
+    // deleteReview
+} = require('../controllers/reviews');
+
+const Review = require('../models/Review');
+
+const router = express.Router({ mergeParams: true });
+
+const advancedResults = require('../middleware/advancedresults');
+const { protect, authorize } = require('../middleware/auth');
+
+router.use(protect);
+router.use(authorize('admin'));
+
+router
+    .route('/')
+    .get(advancedResults(Review, {
+        path: 'bootcamp',
+        select: 'name description'
+        
+    }), getReviews)
+    // .post(createUser);
+
+// router
+//     .route('/:id')
+//     .get(getReview)
+//     .put(updateReview)
+//     .delete(deleteReview);
+
+module.exports = router;
